e566f0889457f9f01fe05c4b9b8983d4021349cd
jvanleersum <75987870+jvanleersum@users.noreply.github.com>
2021-11-23 11:44:14 +0100

    Cleaned makefile and commented out part of trainer.py

--- ai_dj/trainer.py
+++ ai_dj/trainer.py
@@ -172,70 +172,70 @@ def mix_tracks(new_song, other_song):
             continue
     return mixed_song, mix_tracks_rating_df
 
-def get_mix(youtube_link, start):
-    clean_local_folders()
-    audio_feature_track_names = get_audio_features_db()
-    if not audio_feature_track_names["youtube_link"].isin([f'{youtube_link}-{start}']).any():
-        title, output_filename = extract_wav_from_yt_link(youtube_link)
-        title = f'{title}-{start}'
-        print(title)
-        new_song = update_new_audio_features(output_filename, title)
-        np.save(
-         file_io.FileIO(
-             f'gs://ai_dj_batch627_data/data/audio_features/{title}.npy',
-             'w'), new_song)
-        track_info = {"name": title, 
-                      "youtube_link": f'{youtube_link}-{start}',
-                      "audio_features_file": f'gs://ai_dj_batch627_data/data/audio_features/{title}.npy'
-                      }
-        audio_feature_track_names = audio_feature_track_names.append(track_info, ignore_index=True)
-        # print(audio_feature_track_names["youtube_link"])
-        np.save(
-         file_io.FileIO(
-             f'gs://ai_dj_batch627_data/data/audio_features/audio_features_track_names.csv',
-             'w'), audio_feature_track_names)
-        name = title
-    else:
-        print(audio_feature_track_names.head())
+# def get_mix(youtube_link, start):
+#     clean_local_folders()
+#     audio_feature_track_names = get_audio_features_db()
+#     if not audio_feature_track_names["youtube_link"].isin([f'{youtube_link}-{start}']).any():
+#         title, output_filename = extract_wav_from_yt_link(youtube_link)
+#         title = f'{title}-{start}'
+#         print(title)
+#         new_song = update_new_audio_features(output_filename, title)
+#         np.save(
+#          file_io.FileIO(
+#              f'gs://ai_dj_batch627_data/data/audio_features/{title}.npy',
+#              'w'), new_song)
+#         track_info = {"name": title, 
+#                       "youtube_link": f'{youtube_link}-{start}',
+#                       "audio_features_file": f'gs://ai_dj_batch627_data/data/audio_features/{title}.npy'
+#                       }
+#         audio_feature_track_names = audio_feature_track_names.append(track_info, ignore_index=True)
+#         # print(audio_feature_track_names["youtube_link"])
+#         np.save(
+#          file_io.FileIO(
+#              f'gs://ai_dj_batch627_data/data/audio_features/audio_features_track_names.csv',
+#              'w'), audio_feature_track_names)
+#         name = title
+#     else:
+#         print(audio_feature_track_names.head())
 
-        name = audio_feature_track_names[audio_feature_track_names["youtube_link"] == youtube_link]["name"].values[0]
-        print(name)
-        new_song = get_audio_features(name)
-    model = pickle.load(open("pipeline.pkl","rb"))
-    predicted_rating = 0
-    n = 0
-    while predicted_rating < 5.0:
-        if n < 5:
-            # other_name = name
-            # while other_name == name:
-            other_name = audio_feature_track_names.sample(1)["name"].values[0]
-            print(other_name)
-            other_song = get_audio_features(other_name)
-            # audio_files = [name, other_song]
-            mixed_song, mix_tracks_rating_df = mix_tracks(new_song, other_song)
-            mix_tracks_predict_df = mix_tracks_rating_df[["bpm_difference", "camelot_distance", "z_cross_diff_original",
-                "mean_ampl_diff_original", "min_freq_diff_original", "max_freq_diff_original",
-                "range_freq_diff_original", "n_drums", "n_bass", "n_vocals", "n_other", 
-                "mean_ampl_mix", "z_cross_mix"]]
-            predicted_rating = model.predict(mix_tracks_predict_df)[0]
-            print(predicted_rating)
-            n += 1
-        else:
-            break
-    final_mix = np.array(mixed_song)
-    # export a mix to a wav file
-    # final_mix = np.array(final_mix)
-    sr = 44100
-    mixed_name = f"{name} - {other_name}"
-    if os.path.isdir(f'{params.MIXED_AUDIO_FOLDER}'):
-        shutil.rmtree(f'{params.MIXED_AUDIO_FOLDER}')
-        os.mkdir(f'{params.MIXED_AUDIO_FOLDER}/')
+#         name = audio_feature_track_names[audio_feature_track_names["youtube_link"] == youtube_link]["name"].values[0]
+#         print(name)
+#         new_song = get_audio_features(name)
+#     model = pickle.load(open("pipeline.pkl","rb"))
+#     predicted_rating = 0
+#     n = 0
+#     while predicted_rating < 5.0:
+#         if n < 5:
+#             # other_name = name
+#             # while other_name == name:
+#             other_name = audio_feature_track_names.sample(1)["name"].values[0]
+#             print(other_name)
+#             other_song = get_audio_features(other_name)
+#             # audio_files = [name, other_song]
+#             mixed_song, mix_tracks_rating_df = mix_tracks(new_song, other_song)
+#             mix_tracks_predict_df = mix_tracks_rating_df[["bpm_difference", "camelot_distance", "z_cross_diff_original",
+#                 "mean_ampl_diff_original", "min_freq_diff_original", "max_freq_diff_original",
+#                 "range_freq_diff_original", "n_drums", "n_bass", "n_vocals", "n_other", 
+#                 "mean_ampl_mix", "z_cross_mix"]]
+#             predicted_rating = model.predict(mix_tracks_predict_df)[0]
+#             print(predicted_rating)
+#             n += 1
+#         else:
+#             break
+#     final_mix = np.array(mixed_song)
+#     # export a mix to a wav file
+#     # final_mix = np.array(final_mix)
+#     sr = 44100
+#     mixed_name = f"{name} - {other_name}"
+#     if os.path.isdir(f'{params.MIXED_AUDIO_FOLDER}'):
+#         shutil.rmtree(f'{params.MIXED_AUDIO_FOLDER}')
+#         os.mkdir(f'{params.MIXED_AUDIO_FOLDER}/')
     
-    write(f"{params.MIXED_AUDIO_FOLDER}{mixed_name}.wav", sr, final_mix)
-    gcp_storage.upload_mixed_audio(f'{mixed_name}.wav')
-    mix_file = f'{params.MIXED_FOLDER}/{mixed_name}.wav'
-    mix_rating_df = mix_tracks_rating_df.drop(columns=["mix"])
-    return mix_file, mixed_name, mix_rating_df
+#     write(f"{params.MIXED_AUDIO_FOLDER}{mixed_name}.wav", sr, final_mix)
+#     gcp_storage.upload_mixed_audio(f'{mixed_name}.wav')
+#     mix_file = f'{params.MIXED_FOLDER}/{mixed_name}.wav'
+#     mix_rating_df = mix_tracks_rating_df.drop(columns=["mix"])
+#     return mix_file, mixed_name, mix_rating_df
     
 def update_model_with_rating(rating, mix_rating_df):
     df = load_rated_mixes()
